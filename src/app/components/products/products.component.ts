d2957c1dfbe31f3f8afe9a4e52723e1db82f94c7
Favio Andrada <info@fdas.com.ar>
2020-03-22 01:26:16 -0300

    version of pagination on datatable working

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import{ ProductsService} from './../../services/products.service';
 import { Product } from 'src/app/models/products';
+import {MatTableDataSource /*, MatTableModule*/} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
 
 
 @Component({
@@ -10,7 +13,12 @@ import { Product } from 'src/app/models/products';
 })
 export class ProductsComponent implements OnInit {
 
+  loadOK: boolean =false;
   products: Product[]=[];
+  listData: MatTableDataSource<Product>;
+  displayedColumns: string[] = ["Codigo", "Nombre", "Descripcion", "RutaImagen", "PrecioVenta", "urlEmail"]; //'Id', "Codigo", "Nombre", "Descripcion", "RutaImagen", "PrecioVenta"];"urlEmail"
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
   constructor(public productsService: ProductsService ) { }
 
@@ -19,12 +27,22 @@ export class ProductsComponent implements OnInit {
     this.productsService.getData().subscribe(
       products=>{ 
         this.products=products;
-        console.log(products)
+       // console.log(products)
+       this.listData = new MatTableDataSource(this.products);
+       this.listData.paginator = this.paginator;
+      // this.loadOK=true;
       }
       ,
-      err => console.log(err)
+      err => {console.log(err); } //this.loadOK=false;}
     )
 
+    
+    //console.log(this.listData);
   }
+
+  /* por las dudas si no funciona online...
+  ngAfterViewInit() {
+    this.listData.paginator = this.paginator
+  }*/
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,25 +1,21 @@
 import { NgModule } from '@angular/core';
 
-import {     MatButtonModule
-} from '@angular/material/button';
-
-import {     MatCardModule
-} from '@angular/material/card';
-
+import {     MatButtonModule} from '@angular/material/button';
+import {     MatCardModule } from '@angular/material/card';
 import {     MatToolbarModule} from '@angular/material/toolbar';
-
+import {     MatTableModule } from '@angular/material/table';
+import {     MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
     imports:
     [
-        MatButtonModule,
-        MatCardModule  ,        MatToolbarModule
+        MatButtonModule,        MatCardModule  ,        MatToolbarModule,
+        MatTableModule,  MatPaginatorModule,     
     ],
 
     exports: [
-        MatButtonModule,
-        MatCardModule ,
-                MatToolbarModule
+        MatButtonModule,        MatCardModule ,                MatToolbarModule,
+        MatTableModule,  MatPaginatorModule,
     ],
 
     declarations: []

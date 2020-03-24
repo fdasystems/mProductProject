ef524123036bdd9b4cfca70fd4bd604a62fda058
Favio Andrada <info@fdas.com.ar>
2020-03-23 23:14:25 -0300

    Pagination 25

--- src/app/models/pagination.model.ts
+++ src/app/models/pagination.model.ts
@@ -1,5 +1,5 @@
 export class PaginationModel {
-    selectItemsPerPage: number[] = [5]; // [5, 10, 25, 100];
+    selectItemsPerPage: number[] = [25]; // [5, 10, 25, 100];
     pageSize = this.selectItemsPerPage[0];
     pageIndex = 1;
     allItemsLength = 0;
